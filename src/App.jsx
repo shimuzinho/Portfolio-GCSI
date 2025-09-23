b6ac4d25ea575f63dbb93abb8e13e40f96e07c64
GuiHenry17 <gui201629@gmail.com>
2025-09-23 14:48:24 -0400

    feat(title): add title to main page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
+    <>
+    <h1>Portfolio</h1>
     <Spline scene="https://prod.spline.design/Hl7MspNbBvzB031g/scene.splinecode" />
+    </>
   );
 }
