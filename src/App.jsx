8a730c5cffad92cd5f3dc9aaab024e55499583aa
GuiHenry17 <gui201629@gmail.com>
2025-09-30 00:21:18 -0300

    feat(footer): create Footer component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Spline from '@splinetool/react-spline';
 import styles from './styles/app.module.css'
 import Header from './components/Header';
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <div className={styles.containerSegundo}>
         
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/footer.module.css'
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p className={styles.text}>
+                © 2025 Todos os direitos reservados — Guilherme Henrique, Jenifer Porfirio e Pedro Rossini
+            </p>
+        </footer>
+    )
+}
