8df60ebd864fbd802ed9f70a088a37e30b961a91
sixzjkk <rossinipedro613@gmail.com>
2025-09-29 22:49:37 -0400

    feat(app): create initial App component structure with header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import Spline from '@splinetool/react-spline';
+import styles from './styles/app.module.css'
 
 export default function App() {
   return (
-    <>
-    <h1>Portfolio</h1>
-    <Spline scene="https://prod.spline.design/Hl7MspNbBvzB031g/scene.splinecode" />
-    </>
+    <div className={styles.container}>
+      <div className={styles.containerPrimeiro}>
+        <div className={styles.header}>
+          <h1 className={styles.titleHeader}>Portfolio</h1>
+          <div className={styles.nav}>
+            <p className={styles.optionsHeader}>Início</p>
+            <p className={styles.optionsHeader}>Integrantes</p>
+            <p className={styles.optionsHeader}>Projetos</p>
+            <p className={styles.optionsHeader}>Contato</p>
+          </div>
+          <button className={styles.buttonHeader}>Clicar</button>
+        </div>
+        <Spline scene="https://prod.spline.design/Hl7MspNbBvzB031g/scene.splinecode" />
+      </div>
+
+      <div className={styles.containerSegundo}>
+        
+      </div>
+    </div>
   );
 }
